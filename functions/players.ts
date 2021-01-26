494932325e6a661c1b20120311b9a4c4ec6e8148
Ondolin <developer@marvin-kaiser.eu>
2021-01-26 11:29:51 +0100

    added subscribe functions for players (currently no backend support)

--- functions/players.ts
+++ functions/players.ts
@@ -1,6 +1,9 @@
 import {getAllPlayers} from "../backendComunication/entities/player";
 import {Character} from "../dataTypes/Character";
 import {torchly} from "../index";
+import {Player} from "../dataTypes/Player";
+
+let subscriptionCallbacks = <{id: string, callback: Function}[]>[];
 
 function getCharactersByPlayerID(id: string) : Character[] {
     let playerCharacters : Character[] = [];
@@ -13,7 +16,36 @@ function getCharactersByPlayerID(id: string) : Character[] {
     return playerCharacters;
 }
 
+function getPlayerByID(id: string): Player | undefined {
+    return torchly.players.array.find((char) => char.id === id);
+}
+
+function subscribeChanges(id: string, callback: Function) {
+    console.error("Player subscribtions are currently not available!");
+    subscriptionCallbacks.push({id, callback});
+}
+
+function unsubscribeChanges(id: string, callback: Function) {
+    console.error("Player subscribtions are currently not available!");
+    subscriptionCallbacks = subscriptionCallbacks.filter(func => func.id === id && func.callback === callback);
+}
+
+export function dataChanged(playerID: string) {
+    console.error("Player subscribtions are currently not available!");
+    let player = getPlayerByID(playerID);
+
+    subscriptionCallbacks
+        .filter((sub) => sub.id === playerID)
+        .forEach((sub) => sub.callback(player));
+
+    player?.subscriptionCallbacks.forEach((func) => func(player));
+}
+
 export const apiFunctions = {
     getCharactersByPlayerID,
     forceUpdatePlayers: getAllPlayers,
+    getByID: getPlayerByID,
+    subscriptionCallbacks,
+    unsubscribeChanges,
+    dataChanged,
 }
