9b4b4bb8e3bc4205b6890cc95f070f19e35d000e
EricHier <developer.erichier@gmail.com>
2021-01-26 18:52:25 +0100

    turning updateData into async function

--- backendComunication/entities/queryAllData.ts
+++ backendComunication/entities/queryAllData.ts
@@ -4,18 +4,22 @@ import {apolloClient} from "../initialize";
 import {updateData as updateCharacterData} from "./characters/characters";
 import {updateData as updatePlayerData, updateSelf} from "./player";
 
-export function updateData() {
-    apolloClient.query({
-        query: gql`
-            {
-                allCharacters{pos{point{x y} rot size} name token players {id name} id details conditions}
-                me {id name gm}
-                allPlayers {id name gm}
-            }
-        `
-    }).then(({data: {allCharacters, me, allPlayers}}) => {
+export async function updateData() {
+    try {
+        const {data: {allCharacters, me, allPlayers}} = await apolloClient.query({
+            query: gql`
+                {
+                    allCharacters{pos{point{x y} rot size} name token players {id name} id details conditions}
+                    me {id name gm}
+                    allPlayers {id name gm}
+                }
+            `
+        });
+
         updateCharacterData(allCharacters);
         updateSelf(me);
         updatePlayerData(allPlayers);
-    }).catch(logError);
+    } catch (e) {
+        logError(e);
+    }
 }
