c1b43f96b5dfbfcb0fd1576233cb44a9bd97cfa2
EricHier <developer.erichier@gmail.com>
2021-01-26 18:53:16 +0100

    initializing backend communication only once

--- backendComunication/startSubscriptions.ts
+++ backendComunication/startSubscriptions.ts
@@ -1,6 +1,14 @@
 import {subscribeCharacter, subscribeRemoveCharacter} from "./entities/characters/subscribtions";
 
+let initialized = false;
+
 export default function () {
+
+    if(initialized)
+        return;
+
+    initialized = true;
+
     subscribeCharacter();
     subscribeRemoveCharacter();
 }
