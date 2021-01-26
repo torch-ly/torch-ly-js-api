153328aa37ae3ca4efab745a898812c58a410d9d
EricHier <developer.erichier@gmail.com>
2021-01-26 19:01:30 +0100

    adding first test

---
+++ tests/character.test.ts
@@ -0,0 +1,18 @@
+import {defaultConfig, initializeTorchly, torchly} from "../index";
+
+const assert = require('assert');
+
+before(function(done) {
+    initializeTorchly(defaultConfig).then(() => {
+        console.log(1); done()
+    })
+});
+
+describe('character',  () => {
+
+    it('array not empty', () => {
+
+        assert(torchly.characters.array.length >= 0)
+
+    });
+});
