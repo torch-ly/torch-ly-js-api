dc116c3b3dd46d4f58a68502db17422b82e9bf47
Ondolin <developer@marvin-kaiser.eu>
2021-03-11 18:41:46 +0100

    added npm types

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,20 @@ import initializeBackendCommunication from "./backendComunication/initialize";
 import startSubscriptions from "./backendComunication/startSubscriptions";
 import {updateData} from "./backendComunication/entities/queryAllData";
 import {Torchly} from "./dataTypes/Torchly";
+import {Background} from "./dataTypes/Background/Background";
+import {Image} from "./dataTypes/Background/Image";
+import {Circle} from "./dataTypes/Drawings/Circle";
+import {Drawing} from "./dataTypes/Drawings/Drawing";
+import {Line} from "./dataTypes/Drawings/Line";
+import {FogOfWar} from "./dataTypes/FogOfWar/FogOfWar";
+import {Polygon} from "./dataTypes/FogOfWar/Polygon";
+import {Character} from "./dataTypes/Character";
+import {InitiativeValue} from "./dataTypes/InitiativeValue";
+import {Player} from "./dataTypes/Player";
+import {Subscribable} from "./dataTypes/Subscribable";
+import {Viewport} from "./dataTypes/Viewport";
+
+export {Background, Image, Circle, Drawing, Line, FogOfWar, Polygon, Character, InitiativeValue, Player, Subscribable, Viewport, Torchly}
 
 export let torchly: Torchly;
 
